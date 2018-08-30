ecdc0d359f3bcf8cc121c4fcd29d4223db60df35
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-08-30 15:27:07 +0530

    Correctoin

--- cnn2d.py
+++ cnn2d.py
@@ -16,24 +16,23 @@ plt.imshow(X_train[3], cmap='gray')
 plt.show()
 
 
-import numpy
-from keras.datasets import mnist
+
+
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Dense,Dropout,Flatten
+from keras.layers.convolutional import Conv2D,MaxPooling2D
 from keras.utils import np_utils
 
-from keras import backend as K
-K.set_image_dim_ordering('th')
 
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+
+
+
+
+X_train = X_train.reshape(X_train.shape[0], 28, 28,1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0],  28, 28,1).astype('float32')
 
 X_train = X_train / 255
 X_test = X_test / 255
@@ -45,7 +44,7 @@ num_classes = y_test.shape[1]
 
 
 model = Sequential()
-model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+model.add(Conv2D(32, (5, 5), input_shape=(28, 28,1), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 model.add(Flatten())
@@ -57,7 +56,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=20, verbose=2)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("CNN Error: %.2f%%" % (100-scores[1]*100))
